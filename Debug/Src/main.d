370bb6591edcf5d2ca94c194aa115cc75435ff42
camilla28 <css28011997@gmail.com>
2019-04-17 13:33:33 -0300

    Atualizando master

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,94 +1,94 @@
 Src/main.o: ../Src/main.c \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Inc/main.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Inc/stm32f1xx_hal_conf.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Inc/main.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Include/core_cm3.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Include/core_cmInstr.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Include/cmsis_gcc.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Include/core_cmFunc.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can_ex.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
- C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Inc/main.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Inc/stm32f1xx_hal_conf.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Inc/main.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can_ex.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
  ../Src/sd_hal_mpu6050.h
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Inc/main.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Inc/main.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Inc/stm32f1xx_hal_conf.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Inc/stm32f1xx_hal_conf.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Inc/main.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Inc/main.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Include/core_cm3.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Include/core_cm3.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Include/core_cmInstr.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Include/cmsis_gcc.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Include/core_cmFunc.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can_ex.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can_ex.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
 
-C:/Users/befto/Dropbox/Formula/Codigos/Maiume/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
+C:/Users/Camilla/Documents/Formula/STM32/Codigos/MainBoard0504/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
 
 ../Src/sd_hal_mpu6050.h:
